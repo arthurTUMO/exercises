1b3ee6d92c563b1369cf94a47c064689095d312b
Arthur Kasumyan <arthurkasumyan@gmail.com>
2019-11-17 16:25:47 -0500

    four, part five

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -12,24 +12,28 @@
                 <v-text-field
                   label="First Name"
                   v-model="first"
+                  :rules="notBlankRule"
                 ></v-text-field>
               </v-col>
               <v-col cols="12" md="6">
                 <v-text-field
                   label="Last Name"
                   v-model="last"
+                  :rules="notBlankRule"
                 ></v-text-field>
               </v-col>
               <v-col cols="12">
                 <v-text-field
                   label="Email"
                   v-model="email"
+                  :rules="emailRules"
                 ></v-text-field>
               </v-col>
               <v-col cols="12">
                 <v-text-field
                   label="Password"
                   v-model="password"
+                  :rules="passwordRules"
                   :append-icon="show? 'mdi-eye' : 'mdi-eye-off'"
                   :type="show ? 'text' : 'password'"
                   @click:append="show = !show"
@@ -57,7 +61,19 @@ export default {
     first: null,
     last: null,
     email: null,
-    password: null
+    password: null,
+    emailRules: [
+      v => !!v || 'Email is required',
+      v => /.+@.+\..+/.test(v) || 'Email must be valid'
+    ],
+    passwordRules: [
+      v => !!v || 'Password is required',
+      v => (v && v.length >= 8) || 'Password must have 8 or more characters',
+      v => /(?=.*\d)/.test(v) || 'Password must contain at least 1 number'
+    ],
+    notBlankRule: [
+      v => !!v || 'This field is required'
+    ]
   })
 }
 </script>
