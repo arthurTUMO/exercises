b0e609d6003966afdb97dc536d0aa6425f356995
Arthur Kasumyan <arthurkasumyan@gmail.com>
2019-11-17 00:07:18 -0500

    three, part six

--- src/views/ExerciseThree.vue
+++ src/views/ExerciseThree.vue
@@ -31,7 +31,7 @@
     <v-row>
       <v-col cols="12">
         <h2>Part 3</h2>
-        Add a text field below with a label that says "Add students to the list" and a + append-outer-icon.
+        Add a v-text-field below with a label that says "Add students to the list" and a + append-outer-icon.
         <a href="https://vuetifyjs.com/en/components/text-fields#icons" target="_blank">For reference</a>
       </v-col>
       <v-col cols="4">
@@ -57,7 +57,7 @@
         <h2>Part 5</h2>
         Make the students a list of objects instead of a list of strings. Each student
         object should have first and last fields for their <b>անուն</b> and <b>ազգանուն</b>.
-        Does our text-field still update the students correctly?
+        Does our v-text-field still update the students correctly?
       </v-col>
     </v-row>
 
@@ -66,8 +66,32 @@
     <v-row>
       <v-col cols="12">
         <h2>Part 6</h2>
-        Create a form and incorporate the text field we made, as well as another one for last name,
-        so that on submit, it will work with student objects.
+        Create a v-form and incorporate the v-text-field we made, as well as another one for last name,
+        so that on submit, it will work properly with student objects. No model validation needed yet.
+      </v-col>
+    </v-row>
+
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="5">
+            <v-text-field v-model="firstName" label="First Name">
+            </v-text-field>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-text-field v-model="lastName" label="Last Name" append-outer-icon="add" @click:append-outer="addFullNameStudent">
+            </v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+
+    <v-divider class="my-4"/>
+
+    <v-row>
+      <v-col cols="12">
+        <h2>Part 7</h2>
+        Update the code at the top to make the list of students look nicer.
       </v-col>
     </v-row>
 
@@ -79,7 +103,9 @@ export default {
   name: 'ExerciseThree',
 
   data: () => ({
-    students: ['Hayko', 'Mko', 'Abo'],
+    students: [{ first: 'Hayko', last: 'Janyan' },
+      { first: 'Mko', last: 'Kezinchyan' },
+      { first: 'Abo', last: 'Abo' }],
     newStudent: null
   }),
 
@@ -87,6 +113,11 @@ export default {
     addStudent: function () {
       this.students.push(this.newStudent)
       this.newStudent = null
+    },
+    addFullNameStudent: function () {
+      this.students.push({ first: this.firstName, last: this.lastName })
+      this.firstName = null
+      this.lastName = null
     }
   }
 }
