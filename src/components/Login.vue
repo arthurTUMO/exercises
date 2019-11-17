b4810a47caa397c5e1f3039f937f54aa1b65309b
Arthur Kasumyan <arthurkasumyan@gmail.com>
2019-11-17 15:10:11 -0500

    four, part one

---
+++ src/components/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container id="Login">
+    <v-card>
+      <v-container>
+        <v-card-title>
+          Sign in
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  label="Email"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="Password"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary">
+            Sign in
+          </v-btn>
+        </v-card-actions>
+      </v-container>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+
+  data: () => ({
+  })
+}
+</script>
