dfcdb4815446ecf931b360425c96e5300223cb5a
Arthur Kasumyan <arthurkasumyan@gmail.com>
2019-12-15 17:02:30 -0500

    Create exercise 5

---
+++ src/views/ExerciseFive.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container id="exercise-five">
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-text-field label="Text" filled>
+        </v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'ExerciseFive',
+  components: {
+  },
+  data: () => ({
+  })
+}
+</script>
